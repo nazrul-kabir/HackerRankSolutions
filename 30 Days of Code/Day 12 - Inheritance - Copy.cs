6553bbf273df9b33a2d5d4195252a4df5bbd5ea4
Nazrul Kabir <ziankabir@gmail.com>
2021-10-08 22:47:41 +0800

    30 days of Coding day-9,10 added

---
+++ 30 Days of Code/Day 12 - Inheritance - Copy.cs
@@ -0,0 +1,18 @@
+  
+// ========================
+//       Information
+// ========================
+
+// Direct Link: https://www.hackerrank.com/challenges/30-inheritance/problem
+// Difficulty: Easy
+// Max Score: 30
+// Language: C#
+
+// ========================
+//         Solution
+// ========================
+
+
+
+
+
